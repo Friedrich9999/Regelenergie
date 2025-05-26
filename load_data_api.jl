1318c27ff6a2da44f7704917fcd28cde2e7deb84
PeterSteinerIII <126150305+Friedrich9999@users.noreply.github.com>
2025-05-26 23:52:07 +0200

    added api request with writing to database
    Added writing an API Request to a Database. Currently it is only working with Sekundärregelleistung. It is not checked if the data is already in the table

--- load_data_api.jl
+++ load_data_api.jl
@@ -45,55 +45,78 @@ function get_token()
     return pycall(py"get_token", String)
 end
 
-function load_data(token, data, product, start_time, end_time)
-    # Call the Python function from Julia
-    # get OAuth2.0 Token
-    print("loading data $data, from $start_time until $end_time")
+data = "nrvsaldo/AktivierteSRL"
+start_time = "2022-01-01"
+end_time = "2025-12-31"
+product = "Qualitaetsgesichert"
 
-    # get response
-    response = pycall(py"get_data", String, token, data, product, start_time, end_time)
+#function load_data(token, data, product, start_time, end_time)
+# Call the Python function from Julia
+# get OAuth2.0 Token
+print("loading data $data, from $start_time until $end_time")
 
-    #create dataframe
-    df = CSV.File(IOBuffer(response), delim=';', dateformat="dd.mm.yyyy") |> DataFrame
+token = get_token()
 
-    drop = [:Zeitzone, :bis, :Datenkategorie, :Datentyp, :Einheit]
-    select!(df, Not(drop))
+# get response
+response = pycall(py"get_data", String, token, data, product, start_time, end_time)
 
-    rename!(df, [:date, :time, :max_power_50hz, :max_power_amprion, :max_power_tennet, :max_power_transnetBW, :max_power_deutschland, :min_power_50hz, :min_power_amprion, :min_power_tennet, :min_power_transnetBW, :min_power_deutschland])
+#create dataframe
+df = CSV.File(IOBuffer(response), delim=';', dateformat="dd.mm.yyyy") |> DataFrame
 
-    time = Array(df[:, 2])
+time = Array(df[:, 3])
 
-    time = parse_time.(time)
-    df[!, "time"] = time
-    header = names(df)
+time = parse_time.(time) .+ Array(df[:, 1])
 
-    for i in range(3, 12)
-        n = Array(df[:, i])
-        h = header[i]
-        n = parse_comma_float.(n)
-        df[!, header[i]] = n
-    end
+drop = [:Zeitzone, :von, :bis, :Datenkategorie, :Datentyp, :Einheit]
+select!(df, Not(drop))
 
-    start = 1
-    while time[start] != Time(DateTime("00:00", "HH:MM"))
-        start += 1
-    end
+rename!(df, [:date, :max_power_50hz, :max_power_amprion, :max_power_tennet, :max_power_transnetBW, :max_power_deutschland, :min_power_50hz, :min_power_amprion, :min_power_tennet, :min_power_transnetBW, :min_power_deutschland])
 
-    e = length(time)
-    while time[e] != Time(DateTime("23:45", "HH:MM"))
-        e -= 1
-    end
+df[!, "date"] = time
 
-    print("genutzte werte von $start bis $e")
+df
 
-    df = df[start:e, :]
+header = names(df)
 
-    return df
+for i in range(2, 11)
+    n = Array(df[:, i])
+    h = header[i]
+    n = parse_comma_float.(n)
+    df[!, header[i]] = n
 end
 
+start = 1
+while Time(time[start]) != Time(DateTime("00:00", "HH:MM"))
+    start += 1
+end
+
+e = length(time)
+while Time(time[e]) != Time(DateTime("23:45", "HH:MM"))
+    e -= 1
+end
+
+print("genutzte werte von $start bis $e")
+
+df = df[start:e, :]
+
+content_string = "date TEXT"
+for i in header
+    if i != "date"
+        content_string *= ",$i REAL"
+    end
+end
+
+dt = Array(df[:, 1])
+dt = Dates.format.(dt, "yyyy-mm-ddTHH:MM:SS")
+df[!, "date"] = dt
+
+
+content_string
 
 ## Create SQLite Database
 db = SQLite.DB("netztransparenz.db")
 con = DBInterface
-SQLite.execute(db, "CREATE TABLE IF NOT EXISTS test(type,year)")
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS Sekundärregelleistung($content_string)")
 SQLite.tables(db)
+SQLite.load!(df, db, "Sekundärregelleistung")
+df = DataFrame(con.execute(db, "SELECT * FROM Sekundärregelleistung"))
